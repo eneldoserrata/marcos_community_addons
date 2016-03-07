3d8997f242df98c65e6798f79c0caf1737582e89
Eneldo Serrata <eneldoserrata@gmail.com>
2016-03-07 13:57:58 -0400

    read variable header on nginx

--- db_filter/__init__.py
+++ db_filter/__init__.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from openerp import http
+import openerp
 
-db_filter_org = http.db_filter
+import re
 
 
 def db_filter(dbs, httprequest=None):
-    dbs = db_filter_org(dbs, httprequest)
     httprequest = httprequest or http.request.httprequest
-    db_filter_hdr = httprequest.environ.get('HTTP_X_ODOO_DBFILTER')
+    db_filter_hdr = httprequest.environ.get('HTTP_X_ODOO_DBFILTER', False)
+    h = httprequest.environ.get('HTTP_HOST', '').split(':')[0]
+    d, _, r = h.partition('.')
+    if d == "www" and r:
+        d = r.partition('.')[0]
+    r = openerp.tools.config['dbfilter'].replace('%h', h).replace('%d', d)
+    dbs = [i for i in dbs if re.match(r, i)]
     if not dbs and db_filter_hdr:
         dbs = [db_filter_hdr]
     return dbs
