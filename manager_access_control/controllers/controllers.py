17f54b405353e289d61061d48837fde9ab86152d
Eneldo Serrata <eneldoserrata@gmail.com>
2016-03-12 17:57:40 -0400

    redirect to weblogin if not admin user try to get database manager

--- manager_access_control/controllers/controllers.py
+++ manager_access_control/controllers/controllers.py
@@ -2,7 +2,7 @@
 from openerp import http
 from openerp.http import request
 from openerp.addons.web.controllers.main import Database
-
+import werkzeug
 
 
 class Database(Database):
@@ -10,11 +10,11 @@ class Database(Database):
     @http.route('/web/database/manager', type='http', auth="user")
     def manager(self, **kw):
         if not request.env.user or request.env.user.id != 1:
-            return False
+            return werkzeug.utils.redirect("/web/login")
         return self._render_template()
 
     @http.route('/web/database/selector', type='http', auth="none")
     def selector(self, **kw):
         if not request.env.user or request.env.user.id != 1:
-            return False
+            return werkzeug.utils.redirect("/web/login")
         return self._render_template(manage=False)
