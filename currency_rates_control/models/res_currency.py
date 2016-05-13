ff23ec3291746bfd3865fa0f26ad0f9887360cdb
Eneldo Serrata <eneldoserrata@gmail.com>
2016-05-13 16:23:04 -0400

    add 12 digit on rates

--- currency_rates_control/models/res_currency.py
+++ currency_rates_control/models/res_currency.py
@@ -10,5 +10,5 @@ from openerp import models, fields, api
 class res_currency_rate(models.Model):
     _inherit = "res.currency.rate"
 
-    rate = fields.Float('Rate', digits=(12, 8), help='The rate of the currency to the currency of rate 1')
+    rate = fields.Float('Rate', digits=(12, 12), help='The rate of the currency to the currency of rate 1')
     name = fields.Date('Date', required=True, select=True)
