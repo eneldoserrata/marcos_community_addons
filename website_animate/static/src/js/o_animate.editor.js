11f6721a5d634c0bc3c533f0a5b908d4a789462b
Eneldo Serrata <eneldoserrata@gmail.com>
2016-05-15 01:55:26 -0400

    remove contact form form theme

--- website_animate/static/src/js/o_animate.editor.js
+++ website_animate/static/src/js/o_animate.editor.js
@@ -6,11 +6,10 @@ odoo.define('website_animate.o_animate_editor', function (require) {
     //  Animations
     s_options.registry.o_animate = s_options.Class.extend({
         start: function () {
-            this._super();
+            this._super.apply(this, arguments);
             var self = this;
 
             setTimeout(function () {
-
                 if (self.$overlay.find("li.snippet-option-o_animate > ul > li li.active").length > 0) {
                     self.update_options_visibility("show");
                 } else {
@@ -20,16 +19,14 @@ odoo.define('website_animate.o_animate_editor', function (require) {
                 // remove theme_enark animation options from the context menu
                 // WARNING: theme_enark do not prefix js methods,
                 // Remove in Odoo9
-
                 if (self.$overlay.find('li.snippet-option-animation').length > 0) {
                     $(this).addClass("hidden");
                 }
-
-            }, 500)
+            }, 500);
         },
 
         select_class: function (type, value, $li) {
-            this._super(type, value, $li);
+            this._super.apply(this, arguments);
             var self = this;
 
             setTimeout(function () {
@@ -37,15 +34,17 @@ odoo.define('website_animate.o_animate_editor', function (require) {
                 self.$target.css('animation-name', '');
             });
 
-            if (type != "click") {return}
-            if (value.length > 0 ) {
+            if (type !== "click") {
+                return;
+            }
+            if (value.length > 0) {
                 self.update_options_visibility("show");
                 self.$target.removeClass("o_animate_preview");
             } else {
                 setTimeout(function () {
                     self.update_options_visibility("hide");
                     self.$target.removeClass("o_animate_preview o_animate");
-                }, 500)
+                }, 500);
             }
         },
 
@@ -58,21 +57,20 @@ odoo.define('website_animate.o_animate_editor', function (require) {
                 } else if (value == "hide") {
                     self.$overlay.find(opts).addClass("hidden");
                 }
-            })
+            });
         },
 
         clean_for_save: function () {
             var self = this;
 
             // Clean elements
-            self.$target
-            .removeClass("o_animating o_animated o_animate_preview")
-            .css({
-                'animation': '',
-                'animation-name': '',
-                'animation-play-state': '',
-                'visibility': ''
-            });
+            self.$target.removeClass("o_animating o_animated o_animate_preview")
+                        .css({
+                            'animation': '',
+                            'animation-name': '',
+                            'animation-play-state': '',
+                            'visibility': ''
+                        });
             if (self.$target.hasClass("o_animate")) {
                 self.$target.css('animation-play-state', 'paused');
             }
@@ -85,20 +83,19 @@ odoo.define('website_animate.o_animate_editor', function (require) {
     // Duration
     s_options.registry.o_animate_duration = s_options.Class.extend({
         select_class: function (type, value, $li) {
-            this._super(type, value, $li);
+            this._super.apply(this, arguments);
 
             var self = this;
-            var $timeline_duration = self.$overlay.find(".timeline.duration span[simulate='duration']");
-            var $timeline_delay    = self.$overlay.find(".timeline.duration span[simulate='delay']");
+            var $timeline_duration = this.$overlay.find(".timeline.duration span[simulate='duration']");
+            var $timeline_delay = this.$overlay.find(".timeline.duration span[simulate='delay']");
 
-            self.$target
-            .css({
+            this.$target.css({
                 'animation-duration': '',
                 'animation-delay': ''
             });
 
-            var el_delay    = self.$target.css("animation-delay");
-            var el_duration = self.$target.css("animation-duration");
+            var el_delay = this.$target.css("animation-delay");
+            var el_duration = this.$target.css("animation-duration");
             var el_period;
 
             el_delay = parseFloat(el_delay.slice(0,-1));
@@ -108,7 +105,7 @@ odoo.define('website_animate.o_animate_editor', function (require) {
             $timeline_duration.parent().width((el_duration*100)/el_period +"%");
             $timeline_delay.parent().width((el_delay*100)/el_period +"%");
 
-            self.$target.addClass("o_animate_preview").css('animation-name', 'dummy-none').css('animation-duration', '0s');
+            this.$target.addClass("o_animate_preview").css('animation-name', 'dummy-none').css('animation-duration', '0s');
 
             $timeline_duration.css('animation-name', 'dummy-none').css('animation-duration', el_duration + "s").css('animation-delay', el_delay  + "s");
             $timeline_delay.css('animation-name', 'dummy-none').css('animation-duration', el_delay  + "s");
@@ -125,19 +122,18 @@ odoo.define('website_animate.o_animate_editor', function (require) {
     // Delay
     s_options.registry.o_animate_delay = s_options.Class.extend({
         select_class: function (type, value, $li) {
-            this._super(type, value, $li);
+            this._super.apply(this, arguments);
             var self = this;
-            var $timeline_delay = self.$overlay.find(".timeline.delay span[simulate='delay']");
-            var $timeline_duration = self.$overlay.find(".timeline.delay span[simulate='duration']");
+            var $timeline_delay = this.$overlay.find(".timeline.delay span[simulate='delay']");
+            var $timeline_duration = this.$overlay.find(".timeline.delay span[simulate='duration']");
 
-            self.$target
-            .css({
+            this.$target.css({
                 'animation-duration': '',
                 'animation-delay': ''
             });
 
-            var el_delay    = self.$target.css("animation-delay");
-            var el_duration = self.$target.css("animation-duration");
+            var el_delay = this.$target.css("animation-delay");
+            var el_duration = this.$target.css("animation-duration");
             var el_period;
 
             el_delay = parseFloat(el_delay.slice(0,-1));
@@ -147,7 +143,7 @@ odoo.define('website_animate.o_animate_editor', function (require) {
             $timeline_duration.parent().width((el_duration*100)/el_period +"%");
             $timeline_delay.parent().width((el_delay*100)/el_period +"%");
 
-            self.$target.addClass("o_animate_preview").css('animation-name', 'dummy-none').css('animation-duration', '0s');
+            this.$target.addClass("o_animate_preview").css('animation-name', 'dummy-none').css('animation-duration', '0s');
 
             $timeline_duration.css('animation-name', 'dummy-none').css('animation-duration', el_duration + "s").css('animation-delay', el_delay  + "s");
             $timeline_delay.css('animation-name', 'dummy-none').css('animation-duration', el_delay  + "s");
