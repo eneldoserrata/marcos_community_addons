89fc7b8563e2f8ff15922d07a3e4f5cbd90a01a0
Eneldo Serrata <eneldoserrata@gmail.com>
2016-06-21 13:41:03 -0400

    add missing file

---
+++ ferrua/models/sale.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+import re
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    position = fields.Integer(u"Posición")
+
+    @api.onchange("position")
+    def onchange_position(self):
+        if self.position > 0:
+            self.name = "[POS: {}]: {}".format(self.position, self.product_id.name)
+        else:
+            self.name = self.product_id.name
+
