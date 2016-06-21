5fbf9a6e9736ff69d810f926894ab43835627f26
Eneldo Serrata <eneldoserrata@gmail.com>
2016-06-21 13:26:19 -0400

    reorder adendency

--- ferrua/__openerp__.py
+++ ferrua/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product','stock', 'purchase', 'sale'],
+    'depends': ['base','product','sale','stock','purchase'],
 
     # always loaded
     'data': [
