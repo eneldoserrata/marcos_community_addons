52302aac931efcd5932f025e6a12d0e35ed9409a
Eneldo Serrata <eneldoserrata@gmail.com>
2016-08-06 10:07:57 -0400

    IMP on roll sustrato purchase order

--- ferrua/models/purchase.py
+++ ferrua/models/purchase.py
@@ -8,28 +8,50 @@ class PurchaseOrder(models.Model):
 
     @api.one
     def _cal_msi(self):
-        exact = 0
-        master = 0
-        lam = 0
-        for rec in self.roll_order_lines:
-            if rec.product_roll_id.product_tmpl_id.categ_id.extra_info == "exact":
-                exact += rec.msi
-            elif rec.product_roll_id.product_tmpl_id.categ_id.extra_info == "master":
-                master += rec.msi
-            elif rec.product_roll_id.product_tmpl_id.categ_id.extra_info == "lamination":
-                lam += rec.msi
-        self.msi_sub_exact = exact
-        self.msi_sub_master = master
-        self.msi_sub_lam = lam/5
-        self.msi_sub_total = self.msi_sub_exact+self.msi_sub_master+self.msi_sub_lam
+        if self.is_roll_order:
+            exact = 0
+            master = 0
+            lam = 0
+            for rec in self.roll_order_lines:
+                if rec.product_roll_id.product_tmpl_id.categ_id.extra_info == "exact":
+                    exact += rec.msi
+                elif rec.product_roll_id.product_tmpl_id.categ_id.extra_info == "master":
+                    master += rec.msi
+                elif rec.product_roll_id.product_tmpl_id.categ_id.extra_info == "lamination":
+                    lam += rec.msi
+            self.msi_sub_exact = exact
+            self.msi_sub_master = master
+            self.msi_sub_lam = lam/5
+            self.msi_sub_total = self.msi_sub_exact+self.msi_sub_master+self.msi_sub_lam
 
 
-    is_roll_order = fields.Boolean("Pedido de rollos")
-    roll_order_lines = fields.One2many("purchase.order.line.roll", "roll_order_id")
+    is_roll_order = fields.Boolean("Pedido de rollos", copy=True)
+    roll_order_lines = fields.One2many("purchase.order.line.roll", "roll_order_id", copy=True)
     msi_sub_exact = fields.Float("Subtotal Plan Exact", compute=_cal_msi, default=0)
     msi_sub_master = fields.Float("Subtotal Master Rolls", compute=_cal_msi, default=0)
     msi_sub_lam = fields.Float(u"Subtotal Laminación", compute=_cal_msi, default=0)
     msi_sub_total = fields.Float("Total MSI", compute=_cal_msi, default=0)
+
+
+    @api.multi
+    def button_confirm(self):
+
+        for rec in self:
+            if rec.is_roll_order:
+                [line.unlink() for line in rec.order_line]
+                for roll in rec.roll_order_lines:
+                    new_order_line = self.env["purchase.order.line"].new({"product_id": roll.product_roll_id.id,
+                                                                          "order_id": roll.roll_order_id.id})
+                    new_order_line.onchange_product_id()
+                    new_order_line.product_qty = roll.rolls
+                    new_order_line.price_unit = roll.roll_price
+                    new_order_line.create(new_order_line._convert_to_write(new_order_line._cache))
+                    rec._amount_all()
+
+                    
+        return super(PurchaseOrder, self).button_confirm()
+
+
 
 class PurchaseOrderLine(models.Model):
     _name = 'purchase.order.line.roll'
@@ -46,7 +68,7 @@ class PurchaseOrderLine(models.Model):
                 rec.roll_price = rec.total_price_msi/rec.rolls
 
     roll_order_id = fields.Many2one("purchase.order")
-    product_roll_id = fields.Many2one('product.product', string='Product', domain=[('purchase_ok', '=', True),('attribute_value_ids','!=',False)], change_default=True, required=True)
+    product_roll_id = fields.Many2one('product.product', string='Product', domain=[('purchase_ok', '=', True),('categ_id.extra_info','in',('exact','master','lamination'))], change_default=True, required=True)
     rolls = fields.Float("Rollos", default=1)
     ancho = fields.Float(u'Ancho"', compute=_cal_msi)
     largo = fields.Float(u"Largo'", compute=_cal_msi)
@@ -73,20 +95,3 @@ class PurchaseOrderLine(models.Model):
                 return {"value": {"product_roll_id": False},
                         'warning': {'title': u"Error de configuración", 'message': u"El rollo selccionado debe tener las variantes Largo y Ancho definidas"}
                         }
-
-    @api.model
-    def create(self, vals):
-        res =  super(PurchaseOrderLine, self).create(vals)
-        new_order_line = self.env["purchase.order.line"].new({"product_id": res.product_roll_id.id,
-                                                              "order_id": res.roll_order_id.id})
-        new_order_line.onchange_product_id()
-        new_order_line.product_qty = res.rolls
-        new_order_line.price_unit = res.roll_price
-        new_order_line.create(new_order_line._convert_to_write(new_order_line._cache))
-        res.roll_order_id._amount_all()
-        return res
-
-    @api.multi
-    def unlink(self):
-        [rec.unlink() for rec in self.roll_order_id.order_line if rec.product_id.id == self.product_roll_id.id]
-        return super(PurchaseOrderLine, self).unlink()
