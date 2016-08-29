d1a2a6451f27e8676bfb29c2e104332fa91563b4
Eneldo Serrata <eneldoserrata@gmail.com>
2016-08-28 20:33:07 -0400

    added new modules

--- base_phone/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import controller
-from . import res_company
-from . import res_partner
-from . import phone_common
---
+++ hr_recruitment_phone/hr_recruitment_phone.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © 2012-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+from openerp.addons.base_phone.fields import Phone
+
+
+class HrApplicant(models.Model):
+    _inherit = 'hr.applicant'
+    _phone_name_sequence = 50
+
+    partner_phone = Phone(partner_field='partner_id')
+    partner_mobile = Phone(partner_field='partner_id')
+
+    @api.multi
+    def name_get(self):
+        if self._context.get('callerid'):
+            res = []
+            for appl in self:
+                if appl.partner_id:
+                    name = u'%s (%s)' % (appl.partner_id.name, appl.name)
+                elif appl.partner_name:
+                    name = u'%s (%s)' % (appl.partner_name, appl.name)
+                else:
+                    name = appl.name
+                res.append((appl.id, name))
+            return res
+        else:
+            return super(HrApplicant, self).name_get()
