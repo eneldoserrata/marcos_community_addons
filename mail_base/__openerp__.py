67ddad3a0323d4afa5f2f6f326fb252c49098e57
Eneldo Serrata <eneldoserrata@gmail.com>
2016-09-02 06:21:18 -0400

    Add new modules

---
+++ mail_base/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Mail Base",
+    "summary": """Makes Mail extendable""",
+    "category": "Discuss",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    'price': 9.00,
+    'currency': 'EUR',
+
+    "depends": [
+        "base",
+        "mail"
+    ],
+
+    "data": [
+        "views/templates.xml",
+    ],
+    'installable': True,
+}
---
+++ mail_sent/README.rst
@@ -0,0 +1,16 @@
+Sentbox
+=======
+
+Adds Sent menu, which shows sent messages
+
+Usage
+-----
+Click Discuss/Sent menu -- sent messages are displayed
+
+Further information
+-------------------
+Further information and discussion: https://yelizariev.github.io/odoo/module/2015/02/19/sentbox.html
+
+HTML Description: https://apps.odoo.com/apps/modules/9.0/mail_sent/
+
+Tested on Odoo 9.0 b9f206953e3f877adf18643f154d1262842564ee
