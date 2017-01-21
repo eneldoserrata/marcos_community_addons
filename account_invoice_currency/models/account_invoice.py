63e6b5cc484e66f5f1dfbe3ce98efde8f94c49ce
eneldoserrata <eneldoserrata@gmail.com>
2017-01-21 08:27:36 -0400

    add new modules

---
+++ account_invoice_currency/models/account_invoice.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2004-2011 Pexego Sistemas Informáticos. (http://pexego.es)
+# © 2004-2011 Zikzakmedia S.L. (http://zikzakmedia.com)
+#             Jordi Esteve <jesteve@zikzakmedia.com>
+# © 2014-2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    amount_tax_signed = fields.Monetary(
+        string='Tax', currency_field='company_currency_id',
+        store=True, readonly=True, compute='_compute_amount',
+        help="Invoice tax amount in the company currency, "
+             "negative for credit notes.")
+
+    @api.one
+    @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount',
+                 'currency_id', 'company_id')
+    def _compute_amount(self):
+        super(AccountInvoice, self)._compute_amount()
+        self.amount_tax_signed = (
+            self.amount_total_company_signed - self.amount_untaxed_signed)
