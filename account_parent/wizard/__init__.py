bb1b7f3f189a03c081efa7c6acd15187f18e785e
eneldoserrata <eneldoserrata@gmail.com>
2017-03-03 16:21:22 -0400

    add new modules

---
+++ account_parent/wizard/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    ODOO, Open Source Management Solution
+#    Copyright (C) 2016 Steigend IT Solutions
+#    For more details, check COPYRIGHT and LICENSE files
+#
+##############################################################################
+import account_chart
