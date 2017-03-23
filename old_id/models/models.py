7fc507871551b81a86b7152d443bcacc75682798
eneldoserrata <eneldoserrata@gmail.com>
2017-03-22 23:48:06 -0400

    add new addons

--- old_id/models/models.py
+++ old_id/models/models.py
@@ -6,3 +6,15 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     old_id = fields.Integer()
+
+
+class ResPartner(models.Model):
+    _inherit = "product.template"
+
+    old_id = fields.Integer()
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    parent_id = fields.Integer()
