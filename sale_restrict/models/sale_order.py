881c39f0be2ea88e04eebffe14641d2d6ca8c4b4
Eneldo Serrata <eneldoserrata@gmail.com>
2018-07-12 22:28:09 -0400

    add new module

---
+++ sale_restrict/models/sale_order.py
@@ -0,0 +1,19 @@
+# See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models, _
+from odoo.exceptions import Warning
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        zero_price = [x.product_id.name
+                      for x in self.order_line if not x.price_unit]
+
+        if zero_price:
+            message = _("Por favor, especifique el precio unitario para los siguientes productos::") + '\n'
+            message += '\n'.join(map(str, zero_price))
+            raise Warning(message.rstrip())
+        return super(SaleOrder, self).action_confirm()
